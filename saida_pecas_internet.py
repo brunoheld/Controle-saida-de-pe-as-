cc5be53541e87aeb5931d96422096b61763dd732
brunoheld <brunoheld30@gmail.com>
2026-09-13 14:24:20 -0400

    Update saida_pecas_internet.py

--- saida_pecas_internet.py
+++ saida_pecas_internet.py
@@ -14,7 +14,7 @@ if "h_hide" not in st.session_state: st.session_state.h_hide = False
 def l_cli():
     try:
         url = st.secrets["link_planilha"]
-        df = pd.read_csv(f"{url.split('/edit')}/export?format=csv&gid=0")
+        df = pd.read_csv(f"{url.split('/edit')[0]}/export?format=csv&gid=0")
         df.columns = df.columns.str.strip().str.upper()
         return df
     except: return pd.DataFrame(columns=["CLIENTE","ENDERECO","CODELEVADOR"])
@@ -23,7 +23,7 @@ def l_hist():
     if st.session_state.h_hide: return pd.DataFrame()
     try:
         url = st.secrets["link_planilha"]
-        df = pd.read_csv(f"{url.split('/edit')}/export?format=csv&sheet=historico_aceites")
+        df = pd.read_csv(f"{url.split('/edit')[0]}/export?format=csv&sheet=historico_aceites")
         df.columns = df.columns.str.strip().str.upper()
         return df
     except: return pd.DataFrame()
